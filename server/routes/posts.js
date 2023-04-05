32e9a29045dee737473ac9194ec5cd864f07618e
Harsh Singh <harshharsh457@gmail.com>
2023-04-06 00:07:46 +0530

    second commit

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,6 +5,7 @@ import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from 
 const router = express.Router();
 
 router.get('/', getPosts);
+
 router.post('/', createPost);
 router.get('/:id', getPost);
 router.patch('/:id', updatePost);
